1376850e6d9ebc15296812ecd599d8a89b01ca23
antos07 <trotsenko.ant07@gmail.com>
2023-10-29 15:47:20 +0100

    Implement reading grammar from the file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <format>
 #include "grammar.hpp"
 #include "readers.hpp"
 
-int main() {
-    grammar::Grammar grammar{readers::ReadGrammar(std::cin)};
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << std::format("Usage: {} <path to the grammar file> <path to the input file>", argv[0]);
+        return EXIT_FAILURE;
+    }
+    std::ifstream grammarFile{argv[1]};
+    if (!grammarFile.is_open()) {
+        std::perror("The grammar file");
+        return EXIT_FAILURE;
+    }
+
+    grammar::Grammar grammar{readers::ReadGrammar(grammarFile)};
 
     for (const auto &[ruleInput, ruleOutput] : grammar) {
         std::cout << ruleInput << " → " << ruleOutput << '\n';
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
